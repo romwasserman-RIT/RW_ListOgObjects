f328a826c0e83a731ca69c1c2a4db8f3702c66ea
romwasserman-RIT <143665797+romwasserman-RIT@users.noreply.github.com>
2023-09-07 13:22:56 -0400

    Add files via upload

---
+++ RW_ListOgObjects/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RW_ListOgObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
